22ea9734b92e62117df097fe308bb4b5c828d97b
TemiralievS <okillington12@gmail.com>
2023-04-07 15:05:44 +0300

    lesson_6 Models, one-to-one, one-to-many

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
+from sqlalchemy.orm import relationship
 from blog.models.database import db
 from flask_login import UserMixin
 from blog.security import flask_bcrypt
@@ -13,6 +14,7 @@ class User(db.Model, UserMixin):
     _password = Column(LargeBinary, nullable=True)
     first_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
     last_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    author = relationship("Author", uselist=False, back_populates="user")
 
     @property
     def password(self):
