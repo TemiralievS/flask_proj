66546564ab1fabac7f45eca56634af05f4033575
TemiralievS <okillington12@gmail.com>
2023-04-21 14:19:31 +0300

    lesson_10 Flask-COMBO-JSONAPI, ComboJSONAPI, plugins

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,5 +1,7 @@
 from flask_combo_jsonapi import Api
 from combojsonapi.spec import ApiSpecPlugin
+from combojsonapi.event import EventPlugin
+from combojsonapi.permission import PermissionPlugin
 from blog.api.tag import TagList, TagDetail
 from blog.api.user import UserList, UserDetail
 from blog.api.author import AuthorList, AuthorDetail
@@ -20,11 +22,15 @@ def create_api_spec_plugin(app):
 
 
 def init_api(app):
+    event_plugin = EventPlugin()
     api_spec_plugin = create_api_spec_plugin(app)
+    permission_plugin = PermissionPlugin(strict=False)
     api = Api(
         app,
         plugins=[
+            event_plugin,
             api_spec_plugin,
+            permission_plugin,
         ],
     )
 
